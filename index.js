d6d85f1690a7e2a03f794408af7bc1c62ababc62
Evandy <andi_evandy@yahoo.co.id>
2020-03-30 12:43:30 +0700

    add main route

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const mainRouter = require('./routes');
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+
+app.use('/', mainRouter);
 
 app.listen(port, () => console.log(`Todo application listening on port ${port}`));
