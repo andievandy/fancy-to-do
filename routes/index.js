2644bb895de59fc790642c6dcf53a4ad29bf615e
Evandy <andi_evandy@yahoo.co.id>
2020-03-30 12:45:25 +0700

    add todos router

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,7 @@
 const {Router} = require('express');
+const todosRouter = require('./todos');
 const mainRouter = Router();
+
+mainRouter.use('/todos', todosRouter);
 
 module.exports = mainRouter;
---
+++ routes/todos.js
@@ -0,0 +1,4 @@
+const {Router} = require('express');
+const todosRouter = Router();
+
+module.exports = todosRouter;
