1e15a15ccc2254bdb137ef27265eb75abee694bb
Evandy <andi_evandy@yahoo.co.id>
2020-03-30 13:56:32 +0700

    add validation handler to todo router

--- routes/todos.js
+++ routes/todos.js
@@ -1,9 +1,11 @@
 const {Router} = require('express');
 const errorHandler = require('../middlewares/errorhandler');
+const validationHandler = require('../middlewares/validationhandler');
 const TodosController = require('../controllers/todos');
 const todosRouter = Router();
 
 todosRouter.get('/', TodosController.list);
+todosRouter.use(validationHandler);
 todosRouter.use(errorHandler);
 
 module.exports = todosRouter;
