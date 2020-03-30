60545ff70c6e1f318eea0f2ef0908bda4bbe2932
Evandy <andi_evandy@yahoo.co.id>
2020-03-30 13:56:04 +0700

    add validation handler

---
+++ middlewares/validationhandler.js
@@ -0,0 +1,12 @@
+const {ValidationError} = require('sequelize');
+
+function errorHandler(err, req, res, next) {
+    if(err instanceof ValidationError) {
+        let errMsg = err.errors.map(error =>  error.message);
+        res.status(500).json({msg: errMsg});
+    } else {
+        next(err);
+    }
+}
+
+module.exports = errorHandler;
