b18fa94a64b6fff5ebfe5fa9ee0737302e31ee66
Evandy <andi_evandy@yahoo.co.id>
2020-03-30 12:36:41 +0700

    add validation to Todo model

--- models/todo.js
+++ models/todo.js
@@ -1,10 +1,38 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Todo = sequelize.define('Todo', {
-    title: DataTypes.STRING,
-    description: DataTypes.TEXT,
-    status: DataTypes.STRING,
-    due_date: DataTypes.DATE
+    title: {
+      type: DataTypes.STRING,
+      validate: {
+        notEmpty: {
+          msg: "Title is empty"
+        }
+      }
+    },
+    description: {
+      type: DataTypes.TEXT,
+      validate: {
+        notEmpty: {
+          msg: "Description is empty"
+        }
+      }
+    },
+    status: {
+      type: DataTypes.STRING,
+      validate: {
+        notEmpty: {
+          msg: "Status is empty"
+        }
+      }
+    },
+    due_date: {
+      type: DataTypes.DATE,
+      validate: {
+        notEmpty: {
+          msg: "Due date is empty"
+        }
+      }
+    }
   }, {});
   Todo.associate = function(models) {
     // associations can be defined here
