e09e3ab293ee7d0e989538a80254da4e09ce74ec
Evandy <andi_evandy@yahoo.co.id>
2020-03-31 15:38:49 +0700

    rename password check function

--- helpers/password.js
+++ helpers/password.js
@@ -5,8 +5,8 @@ function hashPassword(password) {
     return bcrypt.hashSync(password, hashSalt);
 }
 
-function comparePassword(password, hash) {
+function checkPassword(password, hash) {
     return bcrypt.compareSync(password, hash);
 }
 
-module.exports = { hashPassword, comparePassword };
+module.exports = { hashPassword, checkPassword };
