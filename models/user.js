ae04a9c49c7f54a24e9bfbc8e79cce05b7bd1c7e
Evandy <andi_evandy@yahoo.co.id>
2020-03-31 15:32:33 +0700

    add hook to hash user password

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 'use strict';
+const { hashPassword } = require('../helpers/password');
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
     email: {
@@ -25,7 +26,16 @@ module.exports = (sequelize, DataTypes) => {
         }
       }
     }
-  }, {});
+  }, {
+    hooks: {
+      beforeCreate: (user, options) => {
+        user.password = hashPassword(user.password);
+      },
+      beforeUpdate: (user, options) => {
+        user.password = hashPassword(user.password);
+      }
+    }
+  });
   User.associate = function(models) {
     // associations can be defined here
   };
