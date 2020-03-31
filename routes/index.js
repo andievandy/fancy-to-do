d031fd46482e48ddf95ea9f563bcdd20356c0228
Evandy <andi_evandy@yahoo.co.id>
2020-03-31 14:32:05 +0700

    move error handler in route

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,11 @@
 const {Router} = require('express');
+const errorHandler = require('../middlewares/errorhandler');
+const validationHandler = require('../middlewares/validationhandler');
 const todosRouter = require('./todos');
 const mainRouter = Router();
 
 mainRouter.use('/todos', todosRouter);
+todosRouter.use(validationHandler);
+todosRouter.use(errorHandler);
 
 module.exports = mainRouter;
--- routes/todos.js
+++ routes/todos.js
@@ -1,6 +1,4 @@
 const {Router} = require('express');
-const errorHandler = require('../middlewares/errorhandler');
-const validationHandler = require('../middlewares/validationhandler');
 const TodosController = require('../controllers/todos');
 const todosRouter = Router();
 
@@ -9,7 +7,5 @@ todosRouter.get('/:id', TodosController.getById);
 todosRouter.post('/', TodosController.add);
 todosRouter.put('/:id', TodosController.edit);
 todosRouter.delete('/:id', TodosController.delete);
-todosRouter.use(validationHandler);
-todosRouter.use(errorHandler);
 
 module.exports = todosRouter;
