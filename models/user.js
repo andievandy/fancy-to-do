2a3b55420f6ca589d22d236d1509b963d56d8f00
Evandy <andi_evandy@yahoo.co.id>
2020-03-31 15:32:01 +0700

    add user validation

--- models/user.js
+++ models/user.js
@@ -1,8 +1,30 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
-    email: DataTypes.STRING,
-    password: DataTypes.STRING
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'email is required.'
+        },
+        notEmpty: {
+          msg: 'email is required.'
+        }
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'password is required.'
+        },
+        notEmpty: {
+          msg: 'password is required.'
+        }
+      }
+    }
   }, {});
   User.associate = function(models) {
     // associations can be defined here
