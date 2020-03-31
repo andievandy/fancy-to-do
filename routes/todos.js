6295b159c3b440cecaa38ecc6e29cc3b2435cc46
Evandy <andi_evandy@yahoo.co.id>
2020-03-31 17:08:09 +0700

    add auth and checkuserexist routing

--- routes/todos.js
+++ routes/todos.js
@@ -1,7 +1,10 @@
 const { Router } = require('express');
+const authentication = require('../middlewares/authentication');
+const checkUserExists = require('../middlewares/checkuserexists');
 const TodosController = require('../controllers/todos');
 const todosRouter = Router();
 
+todosRouter.use(authentication, checkUserExists);
 todosRouter.get('/', TodosController.list);
 todosRouter.get('/:id', TodosController.getById);
 todosRouter.post('/', TodosController.add);
