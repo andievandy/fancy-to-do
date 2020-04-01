b760781b9df5d9f6ad2cba6a8e1bc9cb04efc3d9
Evandy <andi_evandy@yahoo.co.id>
2020-04-01 17:55:04 +0700

    client layout

--- client/js/main.js
+++ client/js/main.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
     checkLoginState();
+    getTodoList();
+    $('.sidenav').sidenav();
 });
 
 $('#loginForm').submit(function(e) {
@@ -18,6 +20,7 @@ $('#loginForm').submit(function(e) {
         $('#registerEmail').val('');
         $('#registerPassword').val('');
         checkLoginState();
+        getTodoList();
         showMessage('Login successful');
     }).fail(function(data) {
         showMessage(`Login failed: ${data.responseJSON.errors}`);
@@ -45,47 +48,92 @@ $('#registerForm').submit(function(e) {
     });
 });
 
-$('#btnLogout').click(function() {
+$('.navLogout').click(function() {
     localStorage.removeItem('accessToken');
+    $('#todoList').empty();
     checkLoginState();
 });
 
 $('#btnGetTodoList').click(function() {
-    $.ajax({
-        url: 'http://localhost:3000/todos',
-        method: 'GET',
-        headers: {
-            accessToken: localStorage.getItem('accessToken')
-        }
-    }).done(function(data) {
-        console.log(data);
-        data.forEach(todo => {
-            $('#todoList').append(`<div class="todo"><b>${todo.title}</b><br>${todo.description}</div>`);
-        })
-    }).fail(function(data) {
-        showMessage(`Get Todo List failed: ${data.responseJSON.errors}`);
-    });
+    getTodoList(true);
 });
 
-function checkLoginState(str) {
-    if(localStorage.getItem('accessToken')) {
-        $('#loginForm').hide();
-        $('#registerForm').hide();
-        $('#btnLogout').show();
-    } else {
-        $('#loginForm').show();
-        $('#registerForm').show();
-        $('#btnLogout').hide();
+$('.navRegister').click(function() {
+    showSection('register');
+});
+
+$('.navLogin').click(function() {
+    showSection('login');
+});
+
+$(document).on('click', '.btnTodoEdit', function(e) {
+    let todoId = $(this).parent().parent().data('id');
+});
+
+function getTodoList(force = false) {
+    if(localStorage.getItem('accessToken') || force) {
+        $('#todoList').empty();
+        $.ajax({
+            url: 'http://localhost:3000/todos',
+            method: 'GET',
+            headers: {
+                accessToken: localStorage.getItem('accessToken')
+            }
+        }).done(function(data) {
+            data.forEach(todo => {
+                let html = `
+                    <div class="col s12 m4">
+                        <div class="card todo-item" data-id="${todo.id}">
+                            <div class="card-content">
+                                <span class="card-title">${todo.title}</span>
+                                <p>${todo.description}</p>
+                            </div>
+                            <div class="card-action">
+                                <a href="#" class="btnTodoEdit">Edit</a>
+                                <a href="#" class="btnTodoDelete">Delete</a>
+                            </div>
+                        </div>
+                    </div>
+                `;
+                $('#todoList').append(html);
+            })
+        }).fail(function(data) {
+            showMessage(`Get Todo List failed: ${data.responseJSON.errors}`);
+        });
     }
 }
 
-function appendMessage(str) {
-    let previousText = $('.message').text();
-    $('.message').text(`${previousText}<br>${str}`);
-    $('.message').show();
+function checkLoginState() {
+    if(localStorage.getItem('accessToken')) {
+        showSection('todolist');
+        $('.navRegister').hide();
+        $('.navLogin').hide();
+        $('.navLogout').show();
+    } else {
+        showSection('login');
+        $('.navRegister').show();
+        $('.navLogin').show();
+        $('.navLogout').hide();
+    }
+}
+
+function showSection(page) {
+    $('#sectionLogin').hide();
+    $('#sectionRegister').hide();
+    $('#sectionTodoList').hide();
+    switch(page.toLowerCase()) {
+        case 'login':
+            $('#sectionLogin').show();
+            break;
+        case 'register':
+            $('#sectionRegister').show();
+            break;
+        case 'todolist':
+            $('#sectionTodoList').show();
+            break;
+    }
 }
 
 function showMessage(str) {
-    $('.message').text(str);
-    $('.message').show();
+    M.toast({html: str});
 }
