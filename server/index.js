e27cf6a80aa31ec7167f0346939910db4fbecbbf
Evandy <andi_evandy@yahoo.co.id>
2020-04-01 12:30:44 +0700

    use CORS

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 3000;
 const mainRouter = require('./routes');
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
